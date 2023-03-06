6801473d10b0dcfa4b3db5cde2ebcf8f8102cc6e
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-06 10:14:02 +0100

    Update to Leasing & Room

--- Models/Leasing.cs
+++ Models/Leasing.cs
@@ -5,17 +5,21 @@
         public int LeasingNo { get; set; }
         public DateTime DateTimeFrom { get; set; }
         public DateTime DateTimeTo { get; set; }
+        public int StudentNo { get; set; }
+        public int RoomNo { get; set; }
 
         public Leasing()
         {
             
         }
 
-        public Leasing(int leasingNo, DateTime dateTimeFrom, DateTime dateTimeTo)
+        public Leasing(int leasingNo, DateTime dateTimeFrom, DateTime dateTimeTo, int stundentNo, int roomNo)
         {
             LeasingNo = leasingNo;
             DateTimeFrom = dateTimeFrom;
             DateTimeTo = dateTimeTo;
+            StudentNo = stundentNo;
+            RoomNo = roomNo;
         }
 
     }
--- Models/Room.cs
+++ Models/Room.cs
@@ -5,9 +5,19 @@
         public int RoomNo { get; set; }
         public string Type { get; set; }
         public int Price { get; set; }
+        public int DormitoryNo { get; set; }
+
 
         public Room()
         {
+        }
+
+        public Room(int roomNo, string type, int price, int dormitoryNo)
+        {
+            RoomNo = roomNo;
+            Type = type;
+            Price = price;
+            DormitoryNo = dormitoryNo;
         }
     }
 }
