a4eba74b866c8bc7c6b15d3e94dadb85e3dc72a5
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-06 09:52:17 +0100

    Creation of Leasing & Dormitory

--- Models/Dormitory.cs
+++ Models/Dormitory.cs
@@ -2,5 +2,21 @@
 {
     public class Dormitory
     {
+        public int DormitoryNo { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+
+        public Dormitory(int dormitoryNo, string name, string address)
+        {
+            DormitoryNo = dormitoryNo;
+            Name = name;
+            Address = address;
+        }
+
+        public Dormitory()
+        {
+            
+        }
     }
 }
--- Models/Leasing.cs
+++ Models/Leasing.cs
@@ -2,5 +2,21 @@
 {
     public class Leasing
     {
+        public int LeasingNo { get; set; }
+        public DateTime DateTimeFrom { get; set; }
+        public DateTime DateTimeTo { get; set; }
+
+        public Leasing()
+        {
+            
+        }
+
+        public Leasing(int leasingNo, DateTime dateTimeFrom, DateTime dateTimeTo)
+        {
+            LeasingNo = leasingNo;
+            DateTimeFrom = dateTimeFrom;
+            DateTimeTo = dateTimeTo;
+        }
+
     }
 }
