472656348b6393c3e1c30080fe3981be7f0957ea
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-09 12:45:24 +0100

    Bug?

--- Pages/SharedClass/DormitoryRooms.cshtml.cs
+++ Pages/SharedClass/DormitoryRooms.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RoskildeStudentHousing.Services.Interfaces;
 
 namespace RoskildeStudentHousing.Pages.SharedClass
 {
     public class DormitoryRoomsModel : PageModel
     {
+        IDormitoryService _idormitoryService;
+
+        public IEnumerable<Models.LeasingRoomStudentDorm> leasingRoomStudentDorms { get; set; }
+
+        public DormitoryRoomsModel(IDormitoryService dormitoryService)
+        {
+            _idormitoryService = dormitoryService;
+        }
+
         public void OnGet()
         {
+            //leasingRoomStudentDorms = _idormitoryService.GetAllCollectedInformationFromDorm();
         }
     }
 }
