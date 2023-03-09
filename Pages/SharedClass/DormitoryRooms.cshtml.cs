3fc17507abe47074a27e0745c26f469de641e461
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-09 12:27:28 +0100

    Update too razorpages

---
+++ Pages/SharedClass/DormitoryRooms.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RoskildeStudentHousing.Pages.SharedClass
+{
+    public class DormitoryRoomsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/SharedClass/LeasingStudents.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RoskildeStudentHousing.Pages.SharedClass
+{
+    public class LeasingStudentsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
