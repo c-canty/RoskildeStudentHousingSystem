3bc3e0da2a106613b8b1f38cd82aa32417e08596
Mads Egelund Ludvigsen <mads72q2@edu.zealand.dk>
2023-03-13 10:49:23 +0100

    DateClass

--- Models/DateClass.cs
+++ Models/DateClass.cs
@@ -2,5 +2,18 @@
 {
     public class DateClass
     {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public DateClass()
+        {
+            
+        }
+
+        public DateClass(DateTime startDate, DateTime endDate)
+        {
+            StartDate = startDate;
+            EndDate = endDate;
+        }
     }
 }
