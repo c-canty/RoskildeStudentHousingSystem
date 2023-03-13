6d27633d05a91ec2d29a49c9c55c39c8b52d6aec
c-canty <christian_canty@hotmail.co.uk>
2023-03-13 11:17:41 +0100

    Delete

---
+++ Pages/Student/DeleteStudent.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RoskildeStudentHousing.Models;
+using RoskildeStudentHousing.Services.Interfaces;
+
+namespace RoskildeStudentHousing.Pages.Student
+{
+    public class DeleteStudentModel : PageModel
+    {
+        [BindProperty] public Models.Student student { get; set; }
+
+
+        private IStudentService studentService { get; set; }
+
+        public DeleteStudentModel(IStudentService _studentService)
+        {
+            this.studentService = _studentService;
+        }
+        public void OnGet(string StudentNo)
+        {
+
+            student = studentService.GetStudentById(StudentNo);
+        }
+    }
+}
